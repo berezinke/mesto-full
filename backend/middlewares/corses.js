7a3923192eb9bb7d8bec4e4caed18238e8e46a94
berezinke <berezinke@yandex.ru>
2022-11-16 21:08:06 +0300

    V 1.1

--- backend/middlewares/corses.js
+++ backend/middlewares/corses.js
@@ -9,14 +9,14 @@ module.exports.allowRequest = (req, res, next) => {
   ];
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-  }
-
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
 
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
   next(); // пропускаем запрос дальше
 };
