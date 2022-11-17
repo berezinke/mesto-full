cb9ed62070db3f4c754a4a96ed247e74c61ed4ee
berezinke <berezinke@yandex.ru>
2022-11-17 17:53:40 +0300

    V 1.1

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -6,9 +6,17 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 module.exports.authUser = (req, res, next) => {
   const { Authorization } = req.headers;
 
-  if (!Authorization || !Authorization.startsWith('Bearer ')) {
+  /* if (!Authorization || !Authorization.startsWith('Bearer ')) {
     throw new NotAuthError('Необходима авторизация1');
+  } */
+  if (!Authorization) {
+    throw new NotAuthError('Необходима авторизация11');
   }
+
+  if (!Authorization.startsWith('Bearer ')) {
+    throw new NotAuthError('Необходима авторизация12');
+  }
+
 
   const token = Authorization.replace('Bearer ', '');
   let payload;
