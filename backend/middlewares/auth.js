ff0b8f4519fd7456842d13c6c85a5f90b8695c3b
berezinke <berezinke@yandex.ru>
2022-11-18 13:49:13 +0300

    V 1.1

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -9,38 +9,40 @@ module.exports.authUser = (req, res, next) => {
   /* if (!authorization || !authorization.startsWith('Bearer ')) {
     throw new NotAuthError('Необходима авторизация1');
   } */
-  if (!req.headers) {
-    throw new NotAuthError('Необходима авторизация10');
+  if (!req.user) {
+    if (!req.headers) {
+      throw new NotAuthError('Необходима авторизация10');
+    }
+
+    if (!req.headers.accept) {
+      console.dir(req.headers);
+      throw new NotAuthError('Необходима авторизация1001');
+    }
+
+    if (!req.headers.authorization && ) {
+      // throw new NotAuthError('Необходима авторизация101');
+      throw new NotAuthError('Необходима авторизация1002');
+    }
+
+    if (!authorization) {
+      throw new NotAuthError('Необходима авторизация11');
+    }
+
+    if (!authorization.startsWith('Bearer ')) {
+      throw new NotAuthError('Необходима авторизация12');
+    }
+
+    const token = authorization.replace('Bearer ', '');
+    let payload;
+
+    try {
+      payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'simpleKey');
+    } catch (err) {
+      throw new NotAuthError('Необходима авторизация2');
+    }
+
+    req.user = payload; // записываем пейлоуд в объект запроса
   }
-
-  if (!req.headers.accept) {
-    console.dir(req.headers);
-    throw new NotAuthError('Необходима авторизация1001');
-  }
-
-  if (!req.headers.authorization) {
-    // throw new NotAuthError('Необходима авторизация101');
-    throw new NotAuthError('Необходима авторизация1002');
-  }
-
-  if (!authorization) {
-    throw new NotAuthError('Необходима авторизация11');
-  }
-
-  if (!authorization.startsWith('Bearer ')) {
-    throw new NotAuthError('Необходима авторизация12');
-  }
-
-  const token = authorization.replace('Bearer ', '');
-  let payload;
-
-  try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'simpleKey');
-  } catch (err) {
-    throw new NotAuthError('Необходима авторизация2');
-  }
-
-  req.user = payload; // записываем пейлоуд в объект запроса
 
   next(); // пропускаем запрос дальше
 };
